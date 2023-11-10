309b602c75a456c08d09955c99e991e934e47730
ws245909346 <102574232+ws245909346@users.noreply.github.com>
2023-11-10 16:55:42 +0800

    Update diy-part1.sh

--- scripts/zn-m2/diy-part1.sh
+++ scripts/zn-m2/diy-part1.sh
@@ -11,15 +11,15 @@
 #
 
 # Uncomment a feed source
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
 #echo "src-git kenzok8 https://github.com/kenzok8/small-package.git" >> "feeds.conf.default"
 #echo "src-git small https://github.com/kenzok8/small.git" >> "feeds.conf.default"
-echo 'src-git small https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
+#echo 'src-git small https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
 
 
-git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+#git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 #git clone https://github.com/sirpdboy/luci-app-advanced.git package/advanced
 
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 #git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
