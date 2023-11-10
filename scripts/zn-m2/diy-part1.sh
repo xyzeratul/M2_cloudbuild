142ecf47071c2c88a254e1e424531f6a6f73e20d
ws245909346 <102574232+ws245909346@users.noreply.github.com>
2023-11-11 00:07:49 +0800

    Update diy-part1.sh

--- scripts/zn-m2/diy-part1.sh
+++ scripts/zn-m2/diy-part1.sh
@@ -16,13 +16,16 @@
 #echo "src-git small https://github.com/kenzok8/small.git" >> "feeds.conf.default"
 #echo 'src-git small https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
 
-
+#动态域名解析
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+
+#高级设置含文件管理
 #git clone https://github.com/sirpdboy/luci-app-advanced.git package/advanced
 
-#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+#lede mosdns 5.1.3
+#git clone https://github.com/ws245909346/luci-app-mosdns package/mosdns
 #git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
-git clone https://github.com/ws245909346/luci-app-mosdns package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-
+#SmartDNS官方版本
+git clone https://github.com/pymumu/openwrt-smartdns.git package/smartdns
+git clone https://github.com/pymumu/luci-app-smartdns.git;lede package/luci_smartdns
